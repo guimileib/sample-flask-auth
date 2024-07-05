f7649576ff4e8f73f262c4106d592ceda3e11284
Guilherme Mileib <guilhermemileib@gmail.com>
2024-07-05 15:19:06 -0300

    Criptografia de senha

--- models/user.py
+++ models/user.py
@@ -6,4 +6,4 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True) # nullable é aceitar não ter nenhum registro dentro da coluna ou não ter | True aceita não ter nenhum registro e False não aceita
     password = db.Column(db.String(80), nullable=False) # unique é unicidade do valor no sistema | Exemplo existir apenas um User (Rodrigo)
-    
+    role = db.Column(db.String(80), nullable=False, default='user')
